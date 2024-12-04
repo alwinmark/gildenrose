3ddfa4dcfb0b858853ac06923b17aae9d73bb726
alwinmark <alwin.mark@css.ch>
2024-12-04 11:50:08 +0100

    Test Quality of Normal Items, WellAgedItems and backstagepasses

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,189 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    final static int MAX_QUALITY = 50;
+    final static int MIN_QUALITY = 0;
+
+    @ParameterizedTest
+    @MethodSource("normalItems")
+    void updateQuality_when_updateQualityOfNormalItems_then_decreaseQuality(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality - 1, newItems[0].quality);
+    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void updateQuality_when_updateQualityOfNormalItemsWithMinQuality_then_stayAtMinQuality() {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = new Item("+5 Dexterity Vest", 10, MIN_QUALITY);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(MIN_QUALITY, newItems[0].quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("wellAgedItems")
+    void updateQuality_when_updateQualityOfWellAgedItems_then_increaseQuality(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality + 1, newItems[0].quality);
+    }
+
+    @Test
+    void updateQuality_when_updateQualityOfWellAgedItemsWithMaximumQuality_then_stayAtMaximux() {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = new Item("Aged Brie", 2, MAX_QUALITY);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(MAX_QUALITY, newItems[0].quality);
+    }
+
+    @Test
+    void updateQuality_when_updateQualityOfBackstagePassSellInGreater10_then_IncreaseQualityby1() {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality + 1, newItems[0].quality);
+    }
+
+    @Test
+    void updateQuality_when_updateQualityOfBackstagePassSellInBetween10And5_then_IncreaseQualityby2() {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality + 2, newItems[0].quality);
+    }
+
+    @Test
+    void updateQuality_when_updateQualityOfBackstagePassSellInBetween5And0_then_IncreaseQualityby3() {
+        // arrange
+        Item[] originalItems = new Item[2];
+        originalItems[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+        originalItems[1] = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        for(int i = 0; i < originalItems.length; i++) {
+            assertEquals(originalItems[i].quality + 3, newItems[i].quality);
+        }
+    }
+
+    @Test
+    void updateQuality_when_updateQualityOfBackstagePassSellIn0_then_QualityEquals0() {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(0, newItems[0].quality);
+    }
+
+    public static Item[] deepCloneItems(Item[] original) {
+        Item[] newItems = new Item[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+           Item oi = original[i];
+           newItems[i] = new Item(oi.name, oi.sellIn, oi.quality);
+        }
+
+        return newItems;
+    }
+
+    public static Item[] normalItems() {
+        return new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6)};
+    }
+
+    public static Item[] wellAgedItems() {
+        return new Item[]{
+            new Item("Aged Brie", 2, 0),
+        };
+    }
+
+    public static Item[] backstagePasses() {
+        return new Item[]{
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+        };
     }
 
 }
