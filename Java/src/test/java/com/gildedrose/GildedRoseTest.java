259032bb7a856606c7918cd15f82437a2e745e8e
alwinmark <alwin.mark@css.ch>
2024-12-04 14:40:34 +0100

    Tests before feature complete

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,15 @@
 package com.gildedrose;
 
+import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.platform.commons.util.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -130,7 +137,7 @@ class GildedRoseTest {
         testee.updateQuality();
 
         // assert
-        for(int i = 0; i < originalItems.length; i++) {
+        for (int i = 0; i < originalItems.length; i++) {
             assertEquals(originalItems[i].quality + 3, newItems[i].quality);
         }
     }
@@ -152,12 +159,84 @@ class GildedRoseTest {
         assertEquals(0, newItems[0].quality);
     }
 
+    @ParameterizedTest
+    @MethodSource("sulfuras")
+    void updateQuality_when_updateQualityOfSulfuras_then_nothingChanges(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality, newItems[0].quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("spoiledNormalItems")
+    void updateQuality_when_SellInIsLess0_then_qualityDecreasesDoubles(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality - 2, newItems[0].quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("spoiledWellAgedItems")
+    void updateQuality_when_SellInIsLess0OnWellAgedItems_then_qualityIncreaseDoubles(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].quality + 2, newItems[0].quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("allItemsExceptSulfuras")
+    void updateQuality_when_updateQualities_then_SellInReducedBy1(Item originalItem) {
+        // arrange
+        Item[] originalItems = new Item[1];
+        originalItems[0] = originalItem;
+
+        Item[] newItems = deepCloneItems(originalItems);
+
+        GildedRose testee = new GildedRose(newItems);
+
+        // act
+        testee.updateQuality();
+
+        // assert
+        assertEquals(originalItems[0].sellIn - 1, newItems[0].sellIn);
+    }
+
     public static Item[] deepCloneItems(Item[] original) {
         Item[] newItems = new Item[original.length];
 
         for (int i = 0; i < original.length; i++) {
-           Item oi = original[i];
-           newItems[i] = new Item(oi.name, oi.sellIn, oi.quality);
+            Item oi = original[i];
+            newItems[i] = new Item(oi.name, oi.sellIn, oi.quality);
         }
 
         return newItems;
@@ -169,6 +248,26 @@ class GildedRoseTest {
             new Item("Elixir of the Mongoose", 5, 7), //
             // this conjured item does not work properly yet
             new Item("Conjured Mana Cake", 3, 6)};
+    }
+
+    public static Item[] spoiledNormalItems() {
+        Item[] items = normalItems();
+
+        for (Item item :  items) {
+            item.sellIn = -1;
+        }
+
+        return items;
+    }
+
+    public static Item[] spoiledWellAgedItems() {
+        Item[] items = wellAgedItems();
+
+        for (Item item :  items) {
+            item.sellIn = -1;
+        }
+
+        return items;
     }
 
     public static Item[] wellAgedItems() {
@@ -183,6 +282,34 @@ class GildedRoseTest {
             new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
             new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+        };
+    }
+
+    public static Item[] allItemsExceptSulfuras() {
+        List<Item[]> itemListArray = Arrays.asList(
+            normalItems(),
+            wellAgedItems(),
+            backstagePasses(),
+            spoiledNormalItems(),
+            spoiledWellAgedItems()
+        );
+
+        // List<Item[]>
+        return itemListArray
+            // -> Stream<List<Item[]>
+            .stream()
+            // -> Stream<List<Item>>
+            .map(Arrays::asList)
+            // -> Stream<Item>
+            .flatMap(Collection::stream)
+            // -> Item[]
+            .toArray(Item[]::new);
+    }
+
+    public static Item[] sulfuras() {
+        return new Item[]{
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
         };
     }
 
