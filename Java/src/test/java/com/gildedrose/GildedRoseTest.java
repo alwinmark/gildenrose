8f4ca754ce9797821c3f9984e2867ba68509754e
alwinmark <alwin.mark@css.ch>
2024-12-04 14:46:52 +0100

    Intellij code cleanups

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,15 +1,12 @@
 package com.gildedrose;
 
-import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.platform.commons.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +14,88 @@ class GildedRoseTest {
 
     final static int MAX_QUALITY = 50;
     final static int MIN_QUALITY = 0;
+
+    public static Item[] deepCloneItems(Item[] original) {
+        Item[] newItems = new Item[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+            Item oi = original[i];
+            newItems[i] = new Item(oi.name, oi.sellIn, oi.quality);
+        }
+
+        return newItems;
+    }
+
+    public static Item[] normalItems() {
+        return new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6)};
+    }
+
+    public static Item[] spoiledNormalItems() {
+        Item[] items = normalItems();
+
+        for (Item item : items) {
+            item.sellIn = -1;
+        }
+
+        return items;
+    }
+
+    public static Item[] spoiledWellAgedItems() {
+        Item[] items = wellAgedItems();
+
+        for (Item item : items) {
+            item.sellIn = -1;
+        }
+
+        return items;
+    }
+
+    public static Item[] wellAgedItems() {
+        return new Item[]{
+            new Item("Aged Brie", 2, 0),
+        };
+    }
+
+    public static Item[] backstagePasses() {
+        return new Item[]{
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+        };
+    }
+
+    public static Item[] allItemsExceptSulfuras() {
+        List<Item[]> itemListArray = Arrays.asList(
+            normalItems(),
+            wellAgedItems(),
+            backstagePasses(),
+            spoiledNormalItems(),
+            spoiledWellAgedItems()
+        );
+
+        // List<Item[]>
+        return itemListArray
+            // -> Stream<List<Item[]>
+            .stream()
+            // -> Stream<List<Item>>
+            .map(Arrays::asList)
+            // -> Stream<Item>
+            .flatMap(Collection::stream)
+            // -> Item[]
+            .toArray(Item[]::new);
+    }
+
+    public static Item[] sulfuras() {
+        return new Item[]{
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        };
+    }
 
     @ParameterizedTest
     @MethodSource("normalItems")
@@ -229,88 +308,6 @@ class GildedRoseTest {
 
         // assert
         assertEquals(originalItems[0].sellIn - 1, newItems[0].sellIn);
-    }
-
-    public static Item[] deepCloneItems(Item[] original) {
-        Item[] newItems = new Item[original.length];
-
-        for (int i = 0; i < original.length; i++) {
-            Item oi = original[i];
-            newItems[i] = new Item(oi.name, oi.sellIn, oi.quality);
-        }
-
-        return newItems;
-    }
-
-    public static Item[] normalItems() {
-        return new Item[]{
-            new Item("+5 Dexterity Vest", 10, 20), //
-            new Item("Elixir of the Mongoose", 5, 7), //
-            // this conjured item does not work properly yet
-            new Item("Conjured Mana Cake", 3, 6)};
-    }
-
-    public static Item[] spoiledNormalItems() {
-        Item[] items = normalItems();
-
-        for (Item item :  items) {
-            item.sellIn = -1;
-        }
-
-        return items;
-    }
-
-    public static Item[] spoiledWellAgedItems() {
-        Item[] items = wellAgedItems();
-
-        for (Item item :  items) {
-            item.sellIn = -1;
-        }
-
-        return items;
-    }
-
-    public static Item[] wellAgedItems() {
-        return new Item[]{
-            new Item("Aged Brie", 2, 0),
-        };
-    }
-
-    public static Item[] backstagePasses() {
-        return new Item[]{
-            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
-        };
-    }
-
-    public static Item[] allItemsExceptSulfuras() {
-        List<Item[]> itemListArray = Arrays.asList(
-            normalItems(),
-            wellAgedItems(),
-            backstagePasses(),
-            spoiledNormalItems(),
-            spoiledWellAgedItems()
-        );
-
-        // List<Item[]>
-        return itemListArray
-            // -> Stream<List<Item[]>
-            .stream()
-            // -> Stream<List<Item>>
-            .map(Arrays::asList)
-            // -> Stream<Item>
-            .flatMap(Collection::stream)
-            // -> Item[]
-            .toArray(Item[]::new);
-    }
-
-    public static Item[] sulfuras() {
-        return new Item[]{
-            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-        };
     }
 
 }
